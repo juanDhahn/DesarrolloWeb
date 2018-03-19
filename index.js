77459f901e260ee684f146b28baf79601a06ae3b
Alvaro Gutierrez <alvarogutierrez@MacBook-Pro-de-Alvaro.local>
2018-03-19 14:06:33 -0300

    test server

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var express = require('express');
 // var axios = require('axios');
 var app = express();
 
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World2!');
 });
 
 app.listen(3000, function () {
