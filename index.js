300fe6e3c63cb564c8fff44b02a895c3c67135a9
Alvaro Gutierrez <alvarogutierrez@MacBook-Pro-de-Alvaro.local>
2018-03-19 13:59:59 -0300

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var axios = require('axios');
+// var axios = require('axios');
 var app = express();
 
 app.get('/', function (req, res) {
@@ -10,12 +10,12 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-let url = 'https://la2.api.riotgames.com/lol/summoner/v3/summoners/by-name/Orick?api_key=RGAPI-7ab8a4d9-e0db-490e-a780-581205fe4847';
-
-axios.get(url)
-    .then(function (response) {
-        console.log(response);
-    })
-     .catch(function (error) {
-       console.log(error);
-     });
+// let url = 'https://la2.api.riotgames.com/lol/summoner/v3/summoners/by-name/Orick?api_key=RGAPI-7ab8a4d9-e0db-490e-a780-581205fe4847';
+//
+// axios.get(url)
+//     .then(function (response) {
+//         console.log(response);
+//     })
+//      .catch(function (error) {
+//        console.log(error);
+//      });
