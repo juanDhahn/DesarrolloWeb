a3db305a741065b72d8e69983af4ee203034ee04
Alvaro Gutierrez <alvarogutierrez@MacBook-Pro-de-Alvaro.local>
2018-03-19 11:11:28 -0300

    test server

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+let url = 'https://la2.api.riotgames.com/lol/summoner/v3/summoners/by-name/Orick?api_key=RGAPI-7ab8a4d9-e0db-490e-a780-581205fe4847';
+
+axios.get(url)
+    .then(function (response) {
+        console.log(response);
+    })
+     .catch(function (error) {
+       console.log(error);
+     });
