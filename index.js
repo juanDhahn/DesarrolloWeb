be61881fc7d7ec56c4afc1462aabd80ea024d468
Ubuntu <orick@OrickServer.5ilttxmw0iuexd43czij42h0qd.bx.internal.cloudapp.net>
2018-03-21 23:53:23 +0000

    test server

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-// let url = 'https://la2.api.riotgames.com/lol/summoner/v3/summoners/by-name/Orick?api_key=RGAPI-7ab8a4d9-e0db-490e-a780-581205fe4847';
+// let url = 'https://la2.api.riotgames.com/lol/summoner/v3/summoners/by-name/Orick?api_key=RGAPI-48bc2855-a1a5-442b-8b1c-34f7d38005d3';
 //
 // axios.get(url)
 //     .then(function (response) {
